48438ad34b24ae8b9bb2a68ecc12f7b0b1395e3c
Jiri Tyr <jtyr@users.noreply.github.com>
2021-09-27 23:33:14 +0100

    Adding release process (#28)

--- .github/acm.py
+++ .github/acm.py
@@ -100,7 +100,19 @@ class Tools:
                 raise Exception("cannot close file '%s': %s" % (target_file, e))
 
 
-class Generate:
+class Common:
+    def __init__(self, app, env, meta_path, logger):
+        # Class params
+        self.app = app
+        self.env = env
+        self.meta_path = meta_path
+        self.log = logger
+
+        # Make tools accessible
+        self.tools = Tools(self.log)
+
+
+class Generate(Common):
     def __init__(
         self,
         app,
@@ -113,24 +125,20 @@ class Generate:
         application_template,
         logger,
     ):
+        # Call parent class
+        super().__init__(app, env, meta_path, logger)
+
         # Class params
-        self.app = app
-        self.env = env
         self.zone = zone
         self.meta_path = meta_path
         self.release_path = release_path
         self.template_path = template_path
         self.subscription_template = subscription_template
         self.application_template = application_template
-        self.log = logger
 
         # Data cplaceholders (cache)
         self._params = None
-        self._promotion = None
         self._values = None
-
-        # Make tools accessible
-        self.tools = Tools(self.log)
 
         # Prepare templating
         self.j2e = Environment(
@@ -152,20 +160,6 @@ class Generate:
                 raise Exception("cannot read file 'parameters.yaml': %s" % e)
 
         return self._params
-
-    # Reads promotion.yaml file for particular application
-    def _get_promotion(self):
-        if self._promotion is None:
-            self.log.debug("Reading promotion file")
-
-            try:
-                self._promotion = self.tools.read_yaml_file(
-                    os.path.join(self.meta_path, self.app, "promotion.yaml"),
-                )
-            except Exception as e:
-                raise Exception("cannot read file 'promotion.yaml': %s" % e)
-
-        return self._promotion
 
     # Reads values file for particular application, environment and zone
     def _get_values(self):
@@ -189,8 +183,22 @@ class Generate:
         return self._values
 
     # Custom Jinja2 filter that allows to merge two dicts
-    def _jinja2_filter_merge(self, data, deep=False):
-        pass
+    def _jinja2_filter_merge(self, dict1, dict2, deep=False):
+        if not deep:
+            dict1.update(dict2)
+
+            return dict1
+
+        if not isinstance(dict1, dict) or not isinstance(dict2, dict):
+            return dict2
+
+        for k in dict2:
+            if k in dict1:
+                dict1[k] = self._jinja2_filter_merge(dict1[k], dict2[k], deep=deep)
+            else:
+                dict1[k] = dict2[k]
+
+        return dict1
 
     # Custom Jinja2 filter that allows to produce YAML string
     def _jinja2_filter_to_yaml(self, data):
@@ -205,7 +213,7 @@ class Generate:
 
             sub = template.render(
                 app=self.app,
-                placement="TODO",
+                placement="%s-%s" % (self.env, self.zone),
                 params=self._get_params(),
                 values=self._get_values(),
             )
@@ -256,6 +264,105 @@ class Generate:
             )
 
 
+class Get(Common):
+    def __init__(self, app, meta_path, logger, env=None):
+        # Call parent class
+        super().__init__(app, env, meta_path, logger)
+
+    # Reads promotions.yaml file for particular application
+    def _get_promotion(self):
+        self.log.debug("Reading promotion file")
+
+        try:
+            promotion = self.tools.read_yaml_file(
+                os.path.join(self.meta_path, self.app, "promotion.yaml"),
+            )
+        except Exception as e:
+            raise Exception("cannot read file 'promotion.yaml': %s" % e)
+
+        if "promotion" not in promotion:
+            raise Exception(
+                "there is no promotion defined in the file 'promotion.yaml'"
+            )
+
+        return promotion["promotion"]
+
+    # Get list of zones for the particular environment
+    def zones(self):
+        self.log.info("Getting Zones list")
+
+        promotion = self._get_promotion()
+        data = {}
+        found = False
+
+        # Extract zones and sort them by priorities
+        for p in promotion:
+            if "env" in p and p["env"] == self.env and "placements" in p:
+                for info in p["placements"]:
+                    priority = 0
+
+                    if "priority" in info:
+                        priority = info["priority"]
+
+                    if priority not in data:
+                        data[priority] = []
+
+                    data[priority].append(info["name"])
+
+                    if not found:
+                        found = True
+
+            if found:
+                break
+        else:
+            raise Exception(
+                "environment '%s' not present in the 'promotion.yaml' file" % self.env
+            )
+
+        for zones in data.values():
+            print(",".join(zones))
+
+    # Get the name of the first environment
+    def first_env(self):
+        self.log.info("Getting first env")
+
+        promotion = self._get_promotion()
+
+        if len(promotion) == 0:
+            raise Exception("no environment present in the 'promotion.yaml' file")
+
+        if "env" not in promotion[0]:
+            raise Exception("missing environment name in the first promotion item")
+
+        print(promotion[0]["env"])
+
+    # Get the name of the next environment
+    def next_env(self):
+        self.log.info("Getting highet env")
+
+        promotion = self._get_promotion()
+        found = False
+
+        for p in promotion:
+            if found:
+                print(p["env"])
+
+                break
+
+            if "env" in p and p["env"] == self.env:
+                found = True
+        else:
+            if found:
+                self.log.info(
+                    "Environment '%s' was the last one for promotion" % self.env
+                )
+            else:
+                raise Exception(
+                    "environment '%s' not present in the 'promotion.yaml' file"
+                    % self.env
+                )
+
+
 class Validate:
     def __init__(self, logger):
         # Class params
@@ -290,15 +397,6 @@ def parse_args():
         envvar="ACM_META_PATH",
         help="Path to the ACM meta directory (env: ACM_META_PATH). (default: .)",
     )
-    parser.add_argument(
-        "-r",
-        "--release-path",
-        metavar="PATH",
-        required=True,
-        action=EnvDefault,
-        envvar="ACM_RELEASE_PATH",
-        help="Path to the release directory (env: ACM_RELEASE_PATH).",
-    )
 
     subparsers = parser.add_subparsers(help="Actions.")
 
@@ -318,6 +416,15 @@ def parse_args():
     parser_generate.add_argument(
         "zone",
         help="Zone name.",
+    )
+    parser_generate.add_argument(
+        "-r",
+        "--release-path",
+        metavar="PATH",
+        required=True,
+        action=EnvDefault,
+        envvar="ACM_RELEASE_PATH",
+        help="Path to the release directory (env: ACM_RELEASE_PATH).",
     )
     parser_generate.add_argument(
         "-t",
@@ -361,6 +468,60 @@ def parse_args():
         help="Validate metadata.",
     )
     parser_validate.set_defaults(action="validate")
+    parser_validate.add_argument(
+        "-r",
+        "--release-path",
+        metavar="PATH",
+        required=True,
+        action=EnvDefault,
+        envvar="ACM_RELEASE_PATH",
+        help="Path to the release directory (env: ACM_RELEASE_PATH).",
+    )
+
+    parser_get = subparsers.add_parser(
+        "get",
+        help="Get information.",
+    )
+    get_subparsers = parser_get.add_subparsers(help="Actions.")
+    parser_get.set_defaults(action="get")
+
+    parser_get_zones = get_subparsers.add_parser(
+        "zones",
+        help="Get zones list for the environment.",
+    )
+    parser_get_zones.set_defaults(action="get_zones")
+    parser_get_zones.add_argument(
+        "app",
+        help="Name of the application.",
+    )
+    parser_get_zones.add_argument(
+        "env",
+        help="Environment name.",
+    )
+
+    parser_get_next_env = get_subparsers.add_parser(
+        "next-env",
+        help="Get name of the next environment.",
+    )
+    parser_get_next_env.set_defaults(action="get_next_env")
+    parser_get_next_env.add_argument(
+        "app",
+        help="Name of the application.",
+    )
+    parser_get_next_env.add_argument(
+        "env",
+        help="Environment name.",
+    )
+
+    parser_get_first_env = get_subparsers.add_parser(
+        "first-env",
+        help="Get name of the first environment.",
+    )
+    parser_get_first_env.set_defaults(action="get_first_env")
+    parser_get_first_env.add_argument(
+        "app",
+        help="Name of the application.",
+    )
 
     return parser, parser.parse_args()
 
@@ -411,7 +572,46 @@ def main():
         try:
             gen.application()
         except Exception as e:
+            traceback.print_exc(file=sys.stderr)
             log.error("Failed to generate Application: %s" % e)
+            sys.exit(1)
+    elif args.action.startswith("get_zones"):
+        get = Get(
+            app=args.app,
+            env=args.env,
+            meta_path=args.meta_path,
+            logger=log,
+        )
+
+        try:
+            get.zones()
+        except Exception as e:
+            log.error("Failed to get zones list: %s" % e)
+            sys.exit(1)
+    elif args.action == "get_first_env":
+        get = Get(
+            app=args.app,
+            meta_path=args.meta_path,
+            logger=log,
+        )
+
+        try:
+            get.first_env()
+        except Exception as e:
+            log.error("Failed to get name of the first environment: %s" % e)
+            sys.exit(1)
+    elif args.action == "get_next_env":
+        get = Get(
+            app=args.app,
+            env=args.env,
+            meta_path=args.meta_path,
+            logger=log,
+        )
+
+        try:
+            get.next_env()
+        except Exception as e:
+            log.error("Failed to get name of the next environment: %s" % e)
             sys.exit(1)
     elif args.action == "validate":
         val = Validate(
@@ -423,6 +623,16 @@ def main():
         except Exception as e:
             log.error("Failed to run validations: %s" % e)
             sys.exit(1)
+    else:
+        log.error("No '%s' action specified!" % args.action)
+
+        for action in parser._actions:
+            if isinstance(action, argparse._SubParsersAction):
+                for choice, subparser in action.choices.items():
+                    if choice == args.action:
+                        subparser.print_help()
+
+        sys.exit(1)
 
 
 if __name__ == "__main__":
